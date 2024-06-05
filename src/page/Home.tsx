29605017ec5663d844494d07f9330e60abe86c4a
Nicole Olave <nicoleconsuelo@Nicoles-MacBook-Pro.local>
2024-06-05 01:39:42 -0400

    feat(barloader) development loader.tsx/css add home.tsx

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -6,21 +6,33 @@ import "../styles/HomePage.css";
 import { getMovies } from "../services/APIService";
 import Movie from "../models/Movie";
 import MovieList from "../components/movielist";
+import Loader from "../components/LoaderAPI";
 
 //app es un componente de tipo funcion de reactx
+//useState variable de react
 const Home: React.FC = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [buttonLoader, setLoader] = useState<boolean>(false);
 
   useEffect(() => {
     getMovies().then((movies: Movie[]) => {
       setMovies(movies);
     });
   }, []);
+
+  function buttonAlert() {
+    //cuando hagamos click, debe ser true
+    setLoader(true);
+  }
+
   return (
     <div>
       <h1>Cinema Paraíso</h1>
-   {/* {movies} son las peliculas */}
-      <MovieList movies={movies}/>
+      <button onClick={buttonAlert}>{"Loading..."}</button>
+      {buttonLoader === true && <Loader />}
+
+      {/* {movies} son las peliculas */}
+      <MovieList movies={movies} />
     </div>
   );
 };
